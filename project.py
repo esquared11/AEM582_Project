38a42d5e7d6da10fdd469bd275de4e331874d47a
esquared11 <eli.elstein@gmail.com>
2025-03-26 20:50:46 -0400

    Create project.py
    Added project.py file with intro

---
+++ project.py
@@ -0,0 +1,17 @@
+"""
+Title: AEM582 Space Systems Final Project Spring 2025
+Authors: Jessica Engel, Misael Alvarez, Justin Klein, Eli Elstein
+
+Created: 03/26/25
+Last-Modified: 03/26/25
+
+Inputs:
+Outputs:
+
+"""
+
+# imports
+import astropy
+import sgp4
+
+# main code
