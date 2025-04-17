73393895cb2f07720daa56fae22b568bd5300206
esquared11 <eli.elstein@gmail.com>
2025-04-17 17:03:04 -0400

    GMAT Update
    Got the test script to work. Tomorrow I'll start adding the logic to add in different initial parameters. -- EHE

--- rungmat.py
+++ rungmat.py
@@ -9,7 +9,7 @@ from datetime import datetime
 # main code
 starttime = datetime.now()
 
-args = [r"C:\\PROGRA~1\\GMAT\\bin\\GMAT.exe",
+args = [r"C:\\Users\\eelstein\\GMAT\\bin\\GMAT.exe",
         "--logfile", r"C:\\Users\\eelstein\\Documents\\Bama\\AEM582\\AEM582_Project\\gmatlog.txt",
         "--run", r"C:\\Users\\eelstein\\Documents\\Bama\\AEM582\\AEM582_Project\\test.script"]
 
